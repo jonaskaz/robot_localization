18a8a7d23b2d9fb6f147bed2325211589d13ea67
Jackie Zeng <dolphin.pool01@gmail.com>
2022-10-03 14:37:39 -0400

    deleted extra code.

--- robot_localization/pf.py
+++ robot_localization/pf.py
@@ -348,17 +348,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
-
-
-from matplotlib import pyplot as mp
-import numpy as np
-
-def gaussian(x, mu, sig):
-    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
-
-x_values = np.linspace(-3, 3, 120)
-for mu, sig in [(1, 0.5)]:
-    mp.plot(x_values, gaussian(x_values, mu, sig))
-
-mp.show()
